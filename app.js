3a12a9d16b9e4d88b856d21f99eaacc31c361c32
Jeremy Pollock <jpollock911@gmail.com>
2024-10-06 16:09:53 -0700

    adding build and eslint

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
@@ -12,7 +13,7 @@ app.get('/api/hello', (req, res) => {
 
 // Simple POST route
 app.post('/api/echo', (req, res) => {
-  res.json({ message: 'You sent: ' + JSON.stringify(req.body) });
+  res.json({ message: `You sent: ${JSON.stringify(req.body)}` });
 });
 
 // Start the server
