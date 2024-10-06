b84ab41d1f39f62e81c4104bf5f7e0f894607c00
Jeremy Pollock <jpollock911@gmail.com>
2024-10-06 15:58:12 -0700

    initial commit of api code

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Simple GET route
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+// Simple POST route
+app.post('/api/echo', (req, res) => {
+  res.json({ message: 'You sent: ' + JSON.stringify(req.body) });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
